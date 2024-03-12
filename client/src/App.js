15ab895076fd86eab2c333d25495aa276f4e4589
Anil Kumar Vinnakoti <anil.vinnakoti@gmail.com>
2024-03-13 00:56:25 +0530

    initial commit for FE & BE

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+
+function App() {
+  const {
+    loginWithPopup,
+    loginWithRedirect,
+    user,
+    logout,
+    isAuthenticated,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  async function callProtectedAPI() {
+    try {
+      const token = await getAccessTokenSilently();
+      const res = await axios.get("http://localhost:4000/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>{isAuthenticated ? "Hello Anil" : "Please Login"}</h1>
+      <div>
+        <button onClick={loginWithPopup}>Login With Popup</button>
+      </div>
+      <div>
+        <button onClick={loginWithRedirect}>Login With Redirect</button>
+      </div>
+
+      {isAuthenticated ? (
+        <>
+          <div>
+            <button onClick={callProtectedAPI}>Call Protected</button>
+          </div>
+          <div>
+            <button onClick={logout}>Logout</button>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,58 @@
+// const express = require("express");
+// const cors = require("cors");
+// const { auth } = require("express-oauth2-jwt-bearer");
+// const axios = require("axios");
+import express from "express";
+import cors from "cors";
+import { auth } from "express-oauth2-jwt-bearer";
+import axios from "axios";
+
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(cors());
+const jwtCheck = auth({
+  audience: "its a unique identifier",
+  issuerBaseURL: "https://dev-lokpkdwjbb1xoaby.us.auth0.com/",
+  tokenSigningAlg: "RS256",
+});
+
+// middlewares
+// enforce on all endpoints
+app.use(jwtCheck);
+app.use(express.json());
+
+// routes
+app.get("/me", async (req, res) => {
+  try {
+    const accessToken = req.headers.authorization.split(" ")[1];
+    const response = await axios.get(
+      "https://dev-lokpkdwjbb1xoaby.us.auth0.com/userinfo",
+      {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    const userInfo = response.data;
+    res.send(userInfo);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// error handling
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || "Internal server error";
+  res.status(status).send(message);
+});
+
+app.listen(port, () => console.log("listening on 4000"));
